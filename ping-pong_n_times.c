8a6d4f3a2378403014da8be8bc38f26443d42664
aseremak19 <aleksander.seremak99@gmail.com>
2023-03-06 21:08:50 +0100

    Added timing to not buffer

--- ping-pong_n_times.c
+++ ping-pong_n_times.c
@@ -37,6 +37,7 @@ int main(int argc, char **argv)
             printf("Process %d received message %d from process %d at iteration: %d\n", rank, message, status.MPI_SOURCE, i);
             message = 1;
             MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            i++;
         }
     }
     end_time = MPI_Wtime();

fcd1358b77e08fb9c42d45534baedaa5b800c4b1
aseremak19 <aleksander.seremak99@gmail.com>
2023-03-11 11:23:43 +0100

    New file

---
+++ monte_carlo_circle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int isInCircle()
+{
+    int x;
+    int y;
+    x = rand() % 2;
+    y = rand() % 2;
+    printf
+}
+
+int N = 10000;
+int incircle = 0;
+count = 0;
+
+int main()
+{
+
+    return 0;
+}
---
+++ ping-pong_sync.c
@@ -0,0 +1,91 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getRandom(int lower, int upper)
+{
+    int num = (rand() %
+               (upper - lower + 1)) +
+              lower;
+    // printf("%d ", num);
+    return num;
+}
+
+int main(int argc, char **argv)
+{
+    int rank, size, i;
+    double start_time, end_time, total_time;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (size != 2)
+    {
+        printf("This program requires exactly 2 MPI processes\n");
+        MPI_Finalize();
+        return 1;
+    }
+    int iterations_limit = 10;
+
+    i = 0;
+
+    // start_time = MPI_Wtime();
+
+    // message = 0;
+
+    // MPI_Barrier(MPI_COMM_WORLD);
+    while (i < iterations_limit + 1)
+    {
+        int message[i + 1];
+        int j;
+
+        size_t size = sizeof(message) / sizeof(message[0]);
+        printf("Size of: %d\n ", size);
+
+        for (j = 0; j < size; j++)
+        {
+            message[j] = getRandom(1, 15);
+        }
+
+        if (rank == 0)
+        {
+            // message = 0;
+
+            int j;
+            // printf("Iteraton: %d;  ", i);
+            for (j = 0; j < i + 1; j++)
+            {
+                message[j] = 1;
+                // printf("%d", message[j]);
+            }
+
+            // message++;
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            start_time = MPI_Wtime();
+            MPI_Send(&message, i, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(&message, i, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+            // printf("Process %d received message %d from process %d at iteration: %d\n", rank, *message, status.MPI_SOURCE, i);
+            //   i++;
+            end_time = MPI_Wtime();
+            total_time = (end_time - start_time) / 2;
+            printf("Rank: %d; Total time: %.15f seconds\n", rank, total_time);
+        }
+        else
+        {
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Recv(&message, i, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            // printf("Process %d received message %d from process %d at iteration: %d\n", rank, *message, status.MPI_SOURCE, i);
+            //  message = 1;
+            MPI_Send(&message, i, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            // i++;
+        }
+        i++;
+    }
+    // end_time = MPI_Wtime();
+    // total_time = end_time - start_time;
+    // printf("Rank: %d; Total time: %f seconds\n", rank, total_time);
+    MPI_Finalize();
+    return 0;
+}

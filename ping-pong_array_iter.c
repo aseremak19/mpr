ffa4673c36c0ae7bc6ba4cbd32dd808e895585c3
aseremak19 <aleksander.seremak99@gmail.com>
2023-03-11 13:31:03 +0100

    New file

---
+++ ping-pong_array_iter.c
@@ -0,0 +1,101 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <time.h>
+
+#define ARRAY_SIZE 10
+
+int getRandom(int lower, int upper)
+{
+    int num = (rand() %
+               (upper - lower + 1)) +
+              lower;
+    // printf("%d ", num);
+    return num;
+}
+
+int main(int argc, char **argv)
+{
+
+    // Initialize MPI
+    int rank, size, i;
+    double start_time, end_time, total_time;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (size != 2)
+    {
+        printf("This program requires exactly 2 MPI processes\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    int iteration_limit = 10;
+    int iterations_per_limit = 100;
+
+    int send_0_array[iteration_limit];
+    int recv_1_array[iteration_limit];
+    int recv_0_array[iteration_limit];
+
+    srand(time(0));
+
+    // Set up the array to be sent
+
+    for (i = 0; i < iteration_limit; i++)
+    {
+        send_0_array[i] = i + rank * getRandom(1, 15);
+    }
+
+    i = 0;
+    int j, z;
+    for (i = 0; i <= iteration_limit; i++)
+    {
+        total_time = 0;
+        for (z = 0; z <= iterations_per_limit; z++)
+            // printf("Iteration: %d \niteration limit: %d\n", i, iteration_limit);
+            //  Send the array from rank 0
+            if (rank == 0)
+            {
+                MPI_Barrier(MPI_COMM_WORLD);
+                start_time = MPI_Wtime();
+
+                MPI_Ssend(send_0_array, i, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                MPI_Recv(recv_0_array, i, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                // Measuring and displaying time
+                end_time = MPI_Wtime();
+                total_time = total_time + (end_time - start_time) / 2;
+                // printf("Rank: %d; Total time: %.15f seconds\n", rank, total_time);
+
+                /*printf("Rank %d received array:", rank);
+                for (j = 0; j < i; j++)
+                {
+                    printf(" %d", recv_0_array[j]);
+                }
+                printf("\n");*/
+            }
+            else
+            {
+                MPI_Barrier(MPI_COMM_WORLD);
+                MPI_Recv(recv_1_array, i, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                /*printf("Rank %d received array:", rank);
+                for (j = 0; j < i; j++)
+                {
+                    printf(" %d", recv_1_array[j]);
+                }
+                printf("\n");
+                printf("\n");*/
+
+                MPI_Ssend(recv_1_array, i, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            }
+    }
+    // i++;
+    printf("Rank: %d; Total time: %.15f seconds\n", rank, total_time / (double)iterations_per_limit);
+}
+
+// Finalize MPI
+MPI_Finalize();
+
+return 0;
+}

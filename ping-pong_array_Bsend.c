cb6f37e6a5a249131df9b0bd6760f8b4d5072b7b
aseremak19 <aleksander.seremak99@gmail.com>
2023-03-11 14:24:09 +0100

    New file

--- ping-pong_array_Bsend.c
+++ ping-pong_array_Bsend.c
@@ -8,8 +8,9 @@ int main(int argc, char **argv)
 {
     int rank, size, i, j;
     int send_size, recv_size;
-    int *send_array;
-    int *recv_array;
+    int *send_0_array;
+    int *recv_1_array;
+    int *recv_0_array;
     double start_time, end_time, total_time, avg_time;
 
     int iteration_limit, iteration_per_;
@@ -52,8 +53,9 @@ int main(int argc, char **argv)
     for (send_size = 1; send_size <= iteration_limit; send_size += 1)
     {
         recv_size = send_size;
-        send_array = (int *)malloc(send_size * sizeof(int));
-        recv_array = (int *)malloc(recv_size * sizeof(int));
+        send_0_array = (int *)malloc(send_size * sizeof(int));
+        recv_1_array = (int *)malloc(recv_size * sizeof(int));
+        recv_0_array = (int *)malloc(recv_size * sizeof(int));
 
         buffer_attached_size = MPI_BSEND_OVERHEAD + sizeof(int) * send_size;
         char *buffer_attached = (char *)malloc(buffer_attached_size);
@@ -67,19 +69,19 @@ int main(int argc, char **argv)
                 // Initialize the send array
                 for (j = 0; j < send_size; j++)
                 {
-                    send_array[j] = j + rank * send_size;
+                    send_0_array[j] = j + rank * send_size;
                 }
 
                 start_time = MPI_Wtime();
-                MPI_Bsend(send_array, send_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
-                MPI_Recv(recv_array, recv_size, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Bsend(send_0_array, send_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                MPI_Recv(recv_0_array, recv_size, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 end_time = MPI_Wtime();
                 total_time += end_time - start_time;
             }
             else
             {
-                MPI_Recv(recv_array, recv_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Bsend(send_array, send_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                MPI_Recv(recv_1_array, recv_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Bsend(recv_1_array, recv_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
             }
         }
 
@@ -94,8 +96,9 @@ int main(int argc, char **argv)
         }
 
         // Free the memory
-        free(send_array);
-        free(recv_array);
+        free(send_0_array);
+        free(recv_1_array);
+        free(recv_0_array);
         MPI_Buffer_detach(&buffer_attached, &buffer_attached_size);
         free(buffer_attached);
     }
@@ -105,9 +108,13 @@ int main(int argc, char **argv)
     {
         fclose(file);
     }
+    else
+    {
+    }
 
     // Finalize MPI
-    MPI_Buffer_detach(&recv_array, &send_array, &size);
+    MPI_Buffer_detach(&recv_1_array, &size);
+    MPI_Buffer_detach(&send_0_array, &size);
     MPI_Finalize();
 
     return 0;
